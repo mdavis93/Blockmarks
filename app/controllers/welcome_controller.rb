b7d110648b6161727616261eb076d3b3e288f132
Michael Davis <mdavis93@hotmail.com>
2017-12-27 22:04:07 -0600

    Fixed 'My Bookmark' link bug

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,6 @@
 class WelcomeController < ApplicationController
   def index
-    @first_menu_link_url = bookmarks_path
+    @first_menu_link_url = current_user ? bookmarks_path : new_user_session_path
     @first_menu_link_name = 'My Bookmarks'
     @topics = Topic.includes(bookmarks: [:likes]).all
   end
